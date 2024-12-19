d793ddfe7e04322eb0a39a5fb3c0f04db1105684
ayuwok111 <bahtiaraaganov@gmail.com>
2024-12-19 15:16:35 +0600

    Delete zadan1.py

--- zadan1.py
+++
@@ -1,9 +0,0 @@
-columns = int(input())
-rows = int(input())
-for row in range(1, rows + 1):
-        rowv = []
-        for col in range(1, columns + 1):
-            value = col / row
-            rowv.append(f"{value:.1f}") 
-        print(" ".join(rowv))
-    
